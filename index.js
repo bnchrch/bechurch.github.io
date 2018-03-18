f8bd024a60efc2e09dc7a344def27b0b66915c28
Ben Church <ben.church@metalab.co>
2018-03-18 14:59:21 +0800

    Add transitions

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 particlesJS.load('particles-js', 'particles.json', function() {
   console.log('callback - particles.js config loaded');
 });
+
+const start = () => {
+  const a = document.getElementById("main");
+  console.log("test", a);
+  a.className += " show";
+}
+
+window.onload = start;
